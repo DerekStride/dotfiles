662d5f2d246cb076e4d49227cc41061078fd845f
Derek Stride <derek@derek-mac.local>
2019-03-23 16:34:16 -0400

    Sync with local computer

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -4,7 +4,12 @@
 if $(gls &>/dev/null)
 then
   alias ls="gls -F --color"
-  alias l="gls -lAFh --color"
-  alias ll="gls -lF --color"
-  alias la='gls -AF --color'
+  alias ll="gls -AhlF --color"
+  alias l="gls -hlF --color"
+  alias la='gls -ahlF --color'
 fi
+
+if [[ -d "/usr/local/var/postgres" ]]; then
+  alias psql_start='pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start'
+  alias psql_stop='pg_ctl -D /usr/local/var/postgres stop -s -m fast'
+ fi
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,4 +1,20 @@
-alias reload!='. ~/.zshrc'
 
-alias cls='clear' # Good 'ol Clear Screen command
-alias grep='grep --color'
+# Make Verbose
+alias mkdir='mkdir -p -v'
+alias grep='grep --color=auto'
+
+# Config
+alias less=$PAGER
+
+# Shortcuts
+alias shortcuts='cat ~/.dotfiles/zsh/aliases.zsh; cat ~/.dotfiles/git/aliases.zsh'
+alias Eject='diskutil eject'
+alias List='diskutil list'
+alias reload!='. ~/.zshrc'
+alias myip='curl ip.appspot.com'
+alias g='git'
+
+alias school='cd ~/Dropbox/Documents/School/'
+alias chrome='open /Applications/Google\ Chrome.app'
+alias tivacOpenCD='cd /opt/local/bin/'
+alias tivacConnect='sudo ./openocd -f /opt/local/share/openocd/scripts/board/ek-lm4f120xl.cfg'
