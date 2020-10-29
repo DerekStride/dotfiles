b6464c2bc44bcbcea06f066e5e56db9899484b45
derekstride <djgstride@gmail.com>
2020-10-29 14:56:30 -0400

    Add key bindings for git and fzf commands

---
+++ zsh/key-binding.zsh
@@ -0,0 +1,22 @@
+# Loads the git-fzf file in the functions directory so the _gh functions are loaded
+git-fzf
+
+# A helper function to join multi-line output from fzf
+join-lines() {
+  local item
+  while read item; do
+    echo -n "${(q)item} "
+  done
+}
+
+bind-git-helper() {
+  local c
+  for c in $@; do
+    eval "fzf-g$c-widget() { local result=\$(_g$c | join-lines); zle reset-prompt; LBUFFER+=\$result }"
+    eval "zle -N fzf-g$c-widget"
+    eval "bindkey '^g^$c' fzf-g$c-widget"
+  done
+}
+bind-git-helper f e t r h
+unset -f bind-git-helper
+
