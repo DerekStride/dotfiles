b1f9ed5e93841f450a1db8e63c358da64c7bcf28
derekstride <djgstride@gmail.com>
2020-10-30 15:05:36 -0400

    Fix bug with new glog and setup tmux pane nav using vi keys

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,5 +1,5 @@
 # The rest of my fun git aliases
 alias gl='git pull --prune'
-alias glog="git log --date=short --format="%C(green)%C(bold)%cd %C(auto)%h %an: %s%d" --graph --color=always"
+alias glog="git log --date=short --format='%C(green)%C(bold)%cd %C(auto)%h %an: %s%d' --graph --color=always"
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 
