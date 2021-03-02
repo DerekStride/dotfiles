49e6431f9912190bd04d1c00a40b1631bd41ed31
derekstride <derek.stride@shopify.com>
2021-03-02 10:44:38 -0330

    Better colors, dracula theme

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,75 +1,4 @@
 autoload colors && colors
-# cheers, @ehrenmurdick
-# http://github.com/ehrenmurdick/config/blob/master/zsh/prompt.zsh
-
-if (( $+commands[git] ))
-then
-  git="$commands[git]"
-else
-  git="/usr/bin/git"
-fi
-
-git_branch() {
-  echo $($git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})
-}
-
-git_dirty() {
-  if $(! $git status -s &> /dev/null)
-  then
-    echo ""
-  else
-    if [[ $($git status --porcelain) == "" ]]
-    then
-      echo "on %{$fg_bold[green]%}$(git_prompt_info)%{$reset_color%}"
-    else
-      echo "on %{$fg_bold[red]%}$(git_prompt_info)%{$reset_color%}"
-    fi
-  fi
-}
-
-git_prompt_info () {
- ref=$($git symbolic-ref HEAD 2>/dev/null) || return
-# echo "(%{\e[0;33m%}${ref#refs/heads/}%{\e[0m%})"
- echo "${ref#refs/heads/} "
-}
-
-unpushed () {
-  $git cherry -v @{upstream} 2>/dev/null
-}
-
-need_push () {
-  if [[ $(unpushed) == "" ]]
-  then
-    echo " "
-  else
-    echo " with %{$fg_bold[magenta]%}unpushed%{$reset_color%} "
-  fi
-}
-
-ruby_version() {
-  if (( $+commands[rbenv] ))
-  then
-    echo "$(rbenv version | awk '{print $1}')"
-  fi
-
-  if (( $+commands[rvm-prompt] ))
-  then
-    echo "$(rvm-prompt | awk '{print $1}')"
-  fi
-}
-
-rb_prompt() {
-  if ! [[ -z "$(ruby_version)" ]]
-  then
-    echo "%{$fg_bold[yellow]%}$(ruby_version)%{$reset_color%} "
-  else
-    echo ""
-  fi
-}
-
-return_val() {
-  echo "%{$fg[yellow]%}%?%{$reset_color%} " 
-}
 
 # If the current path, with prefix replacement, has 3 or more elements `%3~`
 # then return the directory we're in `%1/` else return the whole path `%2~`.
@@ -77,12 +6,4 @@ return_val() {
 # The `~` in `%3~` means the current working directory but if it starts with
 # $HOME, that part is replaced by a ‘~’. Changing it to `%3/` would not do the
 # substitution, like we do with `%1/`.
-export PROMPT=$' %{$fg_bold[green]%}%(3~|%1/|%2~)%{$reset_color%} › '
-set_prompt () {
-  export RPROMPT="%{$fg_bold[cyan]%}%{$reset_color%}"
-}
-
-precmd() {
-  title "zsh" "%m" "%55<...<%~"
-  set_prompt
-}
+export PROMPT=$'%{$fg_bold[green]%}➜%{$reset_color%} %{$fg_bold[blue]%}%(3~|%1/|%2~)%{$reset_color%} '
