8863c6f0fa3d856f941f0406c970795cb4aececa
derekstride <derek.stride@shopify.com>
2021-03-04 16:26:12 -0330

    Add bash and lua to tree-sitter config

--- nvim/runtime/lua/treesitter.lua
+++ nvim/runtime/lua/treesitter.lua
@@ -1,5 +1,5 @@
 require('nvim-treesitter.configs').setup {
-  ensure_installed = { 'ruby', 'rust' }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  ensure_installed = { 'ruby', 'rust', 'bash', 'lua' }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   highlight = {
     enable = true,
   },
