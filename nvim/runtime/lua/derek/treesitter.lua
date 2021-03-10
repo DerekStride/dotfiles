3c4faeeaafa7411529e1454d117ded347e0875e8
derekstride <derek.stride@shopify.com>
2021-03-09 20:34:53 -0330

    treesitter configs for hightlighting embedded c

--- nvim/runtime/lua/derek/treesitter.lua
+++ nvim/runtime/lua/derek/treesitter.lua
@@ -1,6 +1,26 @@
+local read_query = function(filename)
+  return table.concat(vim.fn.readfile(vim.fn.expand(filename)), "\n")
+end
+
 require('nvim-treesitter.configs').setup {
-  ensure_installed = { 'ruby', 'rust', 'bash', 'lua' }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  ensure_installed = { 'ruby', 'rust', 'bash', 'lua', 'query' }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   highlight = {
     enable = true,
   },
+  parsers = {
+    lua = {
+      injections = read_query("~/.dotfiles/nvim/tree-sitter/queries/lua/injections.scm"),
+    }
+  },
+  playground = {
+    enable = true,
+    disable = {},
+    updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+    persist_queries = false -- Whether the query persists across vim sessions
+  },
+  query_linter = {
+    enable = true,
+    use_virtual_text = true,
+    lint_events = {"BufWrite", "CursorHold"},
+  }
 }
---
+++ nvim/tree-sitter/queries/lua/injections.scm
@@ -0,0 +1,9 @@
+(
+  (function_call
+    (field_expression
+      (property_identifier) @p_identifier)
+    (arguments
+      (string) @IncludedC)
+  ) @func
+  (eq? @p_identifier "cdef")
+)
