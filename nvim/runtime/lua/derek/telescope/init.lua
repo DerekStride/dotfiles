2def447a0ad7053a53913f9758aee51bc63ce0e0
derekstride <derek.stride@shopify.com>
2021-07-07 10:46:55 -0400

    Dont load telescope until we check that its loaded

--- nvim/runtime/lua/derek/telescope/init.lua
+++ nvim/runtime/lua/derek/telescope/init.lua
@@ -1,7 +1,7 @@
 local has_telescope, telescope = pcall(require, 'telescope')
-local builtin = require('telescope.builtin')
-
 if not has_telescope then return end
+
+local builtin = require('telescope.builtin')
 
 local should_reload = false
 local reloader = function()
