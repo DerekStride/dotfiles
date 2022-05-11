b57325985b80977299da345151d0189e151c3845
derekstride <derek.stride@shopify.com>
2022-05-11 15:41:22 -0400

    Fix completion shortcuts

--- nvim/runtime/lua/derek/completion.lua
+++ nvim/runtime/lua/derek/completion.lua
@@ -11,6 +11,7 @@ g.completion_matching_strategy_list = {'exact', 'substring', 'fuzzy'}
 g.completion_matching_smart_case = 1
 
 local has_cmp, cmp = pcall(require, 'cmp')
+local types = require("cmp.types")
 local lspkind = require('lspkind')
 
 lspkind.init()
@@ -19,6 +20,9 @@ if has_cmp then
   cmp.setup {
     mapping = {
       ['<CR>'] = cmp.mapping.confirm({ select = true }),
+      ['<C-n>'] = cmp.mapping.select_next_item({ behavior = types.cmp.SelectBehavior.Select }),
+      ['<C-p>'] = cmp.mapping.select_prev_item({ behavior = types.cmp.SelectBehavior.Select }),
+      ["<tab>"] = cmp.config.disable,
     },
     sources = {
       { name = 'nvim_lsp' },
@@ -52,11 +56,12 @@ if has_cmp then
         cmp.config.compare.offset,
         cmp.config.compare.exact,
         cmp.config.compare.score,
+        cmp.config.compare.kind,
         -- cmp.config.compare.kind places snippets above other types.
         -- I prefer to have snippets listed last.
-        function(entry1, entry2)
-          return cmp.config.compare.kind(entry2, entry1)
-        end,
+        -- function(entry1, entry2)
+        --   return cmp.config.compare.kind(entry2, entry1)
+        -- end,
         cmp.config.compare.sort_text,
         cmp.config.compare.length,
         cmp.config.compare.order,
