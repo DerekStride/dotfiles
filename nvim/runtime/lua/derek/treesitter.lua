19881c01333ea38fc7e47fdde0f4f99d3c567b70
derekstride <derek.stride@shopify.com>
2023-02-17 15:18:38 -0500

    Local parser example

--- nvim/runtime/lua/derek/treesitter.lua
+++ nvim/runtime/lua/derek/treesitter.lua
@@ -26,3 +26,20 @@ config.setup {
     lint_events = {"BufWrite", "CursorHold"},
   }
 }
+
+-- local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+
+-- parser_config.liquid = {
+--   install_info = {
+--     url = "~/src/github.com/Shopify/tree-sitter-liquid", -- local path or git repo
+--     files = {"src/parser.c", "src/scanner.cc"},
+--     -- optional entries:
+--     branch = "main", -- default branch in case of git repo if different from master
+--     generate_requires_npm = false, -- if stand-alone parser without npm dependencies
+--     requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
+--   },
+--   filetype = "liquid", -- if filetype does not match the parser name
+-- }
+
+-- require("vim.treesitter.query").set_query("liquid", "injections", "(content) @html")
+-- require("vim.treesitter.query").set_query("html", "injections", "(text) @liquid")
