517113901169b5b7403d2f20b295e37883a42cf6
derekstride <derek.stride@shopify.com>
2024-12-12 19:55:12 -0500

    feat: update script template

--- gems/utils/templates/ruby-script-tmpl.rb
+++ gems/utils/templates/ruby-script-tmpl.rb
@@ -6,11 +6,10 @@ require "logger"
 require "json"
 
 $stdout.sync = $stderr.sync = true
-PROGNAME = File.basename($0)
-
-def logger
-  @logger ||= Logger.new($stderr, level: ENV.fetch("LOG_LEVEL", Logger::WARN), progname: PROGNAME)
-end
+def level = ENV.fetch("LOG_LEVEL", Logger::WARN)
+def progname = File.basename($0)
+def logger = @logger ||= Logger.new($stderr, level:, progname:)
+def options = @options ||= {}
 
 gemfile do
   source 'https://rubygems.org'
@@ -18,20 +17,18 @@ gemfile do
   gem "debug"
 end
 
-$options = {}
-
 OptionParser.new do |o|
-  o.banner = "usage: ruby #{PROGNAME} [options]"
+  o.banner = "usage: ruby #{progname} [options]"
   o.on("-v", "--verbose", "Enable verbose logging") do |verbose|
     logger.level -= 1 unless logger.debug?
     logger.level
   end
-end.parse!(ARGV, into: $options)
+end.parse!(ARGV, into: options)
 
-logger.debug("options=#{$options}")
+logger.debug("options=#{options}")
 
 def main
-  puts JSON.dump($options)
+  puts JSON.dump(options)
 end
 
 main
