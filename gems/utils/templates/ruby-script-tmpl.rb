2dc1d59759e393f5d9c9e5b0a4c52b8c1e409e6b
derekstride <derek.stride@shopify.com>
2025-02-07 15:49:49 -0500

    feat: update script tmpl

--- gems/utils/templates/ruby-script-tmpl.rb
+++ gems/utils/templates/ruby-script-tmpl.rb
@@ -1,27 +1,26 @@
 #!/usr/bin/env ruby
 
 require "bundler/inline"
-require "optparse"
 require "logger"
 require "json"
 
 $stdout.sync = $stderr.sync = true
-def level = ENV.fetch("LOG_LEVEL", Logger::WARN)
 def progname = File.basename($0)
-def logger = @logger ||= Logger.new($stderr, level:, progname:)
 def options = @options ||= {}
+def logger = @logger ||= Logger.new($stderr, progname:, level: ENV.fetch("LOG_LEVEL", Logger::WARN))
+def check_exists!(file) = file.exist? ? file : logger.error("File not found: #{file}") && exit(Errno::ENOENT::Errno)
 
 gemfile do
   source 'https://rubygems.org'
 
+  gem "optparse-pathname"
   gem "debug"
 end
 
 OptionParser.new do |o|
   o.banner = "usage: ruby #{progname} [options]"
-  o.on("-v", "--verbose", "Enable verbose logging") do |verbose|
-    logger.level -= 1 unless logger.debug?
-  end
+  o.on("-v", "--verbose", "Enable verbose logging") { logger.level -= 1 }
+  o.on("-f", "--file FILE", Pathname, "A file to process") { check_exists!(_1) }
 end.parse!(ARGV, into: options)
 
 logger.debug("options=#{options}")
