441911a9360153948b1afebfb821bb46a1d90056
derekstride <derek.stride@shopify.com>
2026-03-09 09:20:42 -0400

    feat(zsh): add track alias for sift issue queries
    - add a shell alias to query `.sift/issues.jsonl` with `sq` for faster issue tracking
    - ignore the local `.sift/` directory to avoid committing generated sift data

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,4 +4,5 @@
 alias shortcuts='cat ~/.dotfiles/zsh/aliases.zsh; cat ~/.dotfiles/git/aliases.zsh'
 alias reload!='. ~/.zshrc'
 alias myip='curl ip.appspot.com'
+alias track='sq -q .sift/issues.jsonl'
 
