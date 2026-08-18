579bee7a4a539332ba7adbbaf3f7884e694c422f
derekstride <derek.stride@shopify.com>
2026-08-18 15:43:36 -0400

    chore(zsh): removed obsolete track alias
    - Removed the global Sift issue queue shortcut while preserving the remaining zsh aliases.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,5 +3,3 @@
 # Shortcuts
 alias shortcuts='cat ~/.dotfiles/zsh/aliases.zsh; cat ~/.dotfiles/git/aliases.zsh'
 alias reload!='. ~/.zshrc'
-alias track='sq -q "${XDG_DATA_HOME:-$HOME/.local/share}/sq/issues.jsonl"'
-
